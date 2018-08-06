b1412b533eaa091d7bad31c0baae3143a0b86c3d
mksdfmamunur1 <40561523+mksdfmamunur1@users.noreply.github.com>
2018-08-06 22:02:48 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+// $( '.navbar-nav a' ).on( 'click', function () {
+// 	$( '.navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+// 	$( this ).parent( 'li' ).addClass( 'active' );
+// });
